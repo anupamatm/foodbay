3de8ee0e8b777b8a5b5c58452d7b8037e18fa64b
anupama <anupamaunni612@gmail.com>
2022-10-06 15:39:07 +0530

    home commit

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -4,7 +4,7 @@ from .import views
 urlpatterns = [
 
     path('base', views.base, name='base'),
-    path('home', views.home, name='home'),
+    path('', views.home, name='home'),
     path('menu', views.menu, name='menu'),
     path('about', views.about, name='about'),
     path('contact', views.contact, name='contact'),
